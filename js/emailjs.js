38846d148a4a12e06c77f74878da0dc72cf04bce
heisenberg-meth <nmdevi63@gmail.com>
2025-10-13 14:32:48 +0530

    email.js

--- js/emailjs.js
+++ js/emailjs.js
@@ -1,16 +1,25 @@
-function sendemail() {
-  let params = {
-    name: document.querySelector("#name"),
-    mobile: document.querySelector("#mobile"),
-    mobile: document.querySelector("#email"),
-    message: document.querySelector("#message"),
+function sendEmail(e) {
+  e.preventDefault();
+  const params = {
+    name: document.getElementById("name").value,
+    mobile: document.getElementById("mobile").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
   };
-  emailjs.send("service_ks81605", "template_39p6krl", params).then(
-    (response) => {
+
+  const serviceID = "service_ks81605";
+  const templateID = "template_39p6krl";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((response) => {
+      alert("✅ Message sent successfully!");
       console.log("SUCCESS!", response.status, response.text);
-    },
-    (error) => {
-      console.log("FAILED...", error);
-    }
-  );
+
+      document.querySelector("form").reset();
+    })
+    .catch((error) => {
+      alert("❌ Message failed to send. Try again!");
+      console.error("FAILED...", error);
+    });
 }
