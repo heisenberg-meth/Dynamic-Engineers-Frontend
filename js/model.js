5177d0799530940c41edc53b174f1ea8177853a1
heisenberg-meth <nmdevi63@gmail.com>
2025-10-15 10:47:22 +0530

    model secondary

--- js/model.js
+++ js/model.js
@@ -1,39 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   const modelViewer = document.querySelector("model-viewer");
-  const buttons = document.querySelectorAll(".d-btns button");
+  const buttons = document.querySelectorAll(".toggle-btns .togglebtn");
   const loading = document.getElementById("loading");
-
-  if (buttons.length > 0) buttons[0].classList.add("btn-active");
-
+  if (buttons.length > 0) buttons[0].classList.add("active");
   modelViewer.addEventListener("load", () => {
     loading.style.opacity = "0";
     setTimeout(() => (loading.style.display = "none"), 500);
     modelViewer.classList.remove("fade");
   });
-
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       loading.style.display = "block";
       loading.style.opacity = "1";
       modelViewer.classList.add("fade");
-
       const modelName = btn.textContent.trim();
       const newSrc = `./modal/glb/${modelName}.glb`;
-
       console.log("Loading model:", newSrc);
-
       modelViewer.setAttribute("src", newSrc);
-      buttons.forEach((b) => b.classList.remove("btn-active"));
-      btn.classList.add("btn-active");
+      buttons.forEach((b) => b.classList.remove("active"));
+      btn.classList.add("active");
     });
-  });
-});
-
-const toggles = document.querySelectorAll(".togglebtn");
-
-toggles.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    toggles.forEach((b) => b.classList.remove("active"));
-    btn.classList.add("active");
   });
 });
