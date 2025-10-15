06e49a84b9050eae92d64ce415145243935af7a1
heisenberg-meth <nmdevi63@gmail.com>
2025-10-15 15:54:20 +0530

    footerworks

--- js/emailjs.js
+++ js/emailjs.js
@@ -18,6 +18,7 @@ function sendEmail(form) {
     name: document.getElementById("name").value.trim(),
     mobile: document.getElementById("mobile").value.trim(),
     email: document.getElementById("email").value.trim(),
+    companyname: document.getElementById("companyname").value.trim(),
     message: document.getElementById("message").value.trim(),
   };
 
@@ -28,7 +29,9 @@ function sendEmail(form) {
     .send(serviceID, templateID, params)
     .then((response) => {
       console.log("SUCCESS!", response.status, response.text);
-      alert("Your Message Will Be sent To Dynamic Electrocplaters Private Limited!");
+      alert(
+        "Your Message Will Be sent To Dynamic Electrocplaters Private Limited!"
+      );
       form.reset();
       form.classList.remove("was-validated");
     })
